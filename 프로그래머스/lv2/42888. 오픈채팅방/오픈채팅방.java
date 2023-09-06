c6b0190c3f474a2dd0a2f4c5dd1d1f52c5f0f7b5
Gatsudongchef <118706806+Gatsudongchef@users.noreply.github.com>
2023-09-06 18:52:01 +0900

    [level 2] Title: 오픈채팅방, Time: 176.15 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,35 +1,6 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 class Solution {
-    public List<String> solution(String[] record) {
-        HashMap<String, String> hm = new HashMap<>();
-		List<String> list = new ArrayList<>();
-		List<String> answer = new ArrayList<>();
-
-		for (String s : record) {
-
-			if (s.split(" ")[0].equals("Leave")) {
-				list.add(s.split(" ")[0] + " " + s.split(" ")[1]);
-			} else if (s.split(" ")[0].equals("Enter")) {
-				hm.put(s.split(" ")[1], s.split(" ")[2]);
-				list.add(s.split(" ")[0] + " " + s.split(" ")[1]);
-			} else {
-				hm.put(s.split(" ")[1], s.split(" ")[2]);
-			}
-
-		}
-
-		for (String s : list) {
-			if (s.split(" ")[0].equals("Enter")) {
-				answer.add(hm.get(s.split(" ")[1]) + "님이 들어왔습니다.");
-				// System.out.println(hm.get(s.split(" ")[1]) + "님이 들어왔습니다.");
-			} else {
-				answer.add(hm.get(s.split(" ")[1]) + "님이 나갔습니다.");
-				// System.out.println(hm.get(s.split(" ")[1]) + "님이 나갔습니다.");
-			}
-		}
+    public String[] solution(String[] record) {
+        String[] answer = {};
         return answer;
     }
 }
