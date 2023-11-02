b86fbd2623bbeae744a27f88bd18a22c1a61240b
Gatsudongchef <118706806+Gatsudongchef@users.noreply.github.com>
2023-11-02 14:52:49 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        
+        if (Math.sqrt(n) == (long)Math.sqrt(n)) {
+            return (((long)Math.sqrt(n))+1)*(((long)Math.sqrt(n))+1);
+        }
+        
+        
+        return answer;
+    }
+}
