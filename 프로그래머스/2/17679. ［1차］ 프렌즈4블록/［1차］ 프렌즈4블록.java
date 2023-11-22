30196834974f26d5860909b3f87e995a0a584437
Gatsudongchef <118706806+Gatsudongchef@users.noreply.github.com>
2023-11-22 15:28:53 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 18.29 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        int x, y;
+        char[][] arr = new char[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int k = 0; k < n; k++) {
+                arr[i][k] = board[i].charAt(k);
+            }
+        }
+
+        while (true) {
+            HashSet<String> delete = new HashSet<>();
+
+            for (int k = 0; k < m - 1; k++) {
+                for (int i = 0; i < n - 1; i++) {
+                    if (arr[k][i] != '0' && arr[k][i] == arr[k][i + 1]) {
+                        if (arr[k][i] == arr[k + 1][i] && arr[k + 1][i] == arr[k + 1][i + 1]) {
+                            delete.add("" + k + " " + i);
+                            delete.add("" + k + " " + (i + 1));
+                            delete.add("" + (k + 1) + " " + i);
+                            delete.add("" + (k + 1) + " " + (i + 1));
+                        }
+                    }
+                }
+            }
+
+            if (delete.isEmpty()) {
+                break;
+            }
+
+            answer += delete.size();
+            Iterator<String> iter = delete.iterator();
+
+            while (iter.hasNext()) {
+                String str = iter.next();
+                String[] arrstr = str.split(" ");
+
+                x = Integer.parseInt(arrstr[0]);
+                y = Integer.parseInt(arrstr[1]);
+
+                arr[x][y] = '0';
+            }
+
+            // Gravity: move blocks downward
+            for (int i = m - 1; i >= 0; i--) {
+                for (int k = 0; k < n; k++) {
+                    if (arr[i][k] == '0') {
+                        int idx = i;
+                        while (idx > 0 && arr[idx][k] == '0') {
+                            idx--;
+                        }
+                        arr[i][k] = arr[idx][k];
+                        arr[idx][k] = '0';
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
